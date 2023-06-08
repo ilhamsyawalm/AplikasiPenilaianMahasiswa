4c864427df3ac0d9b02e8985e769ed40243f57a5
Zhephyr <ilhamsyawal87@gmail.com>
2023-06-08 19:49:52 +0700

    tampilan

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
       <table>
         <thead>
-          <th>Mahasiswa Ke-X</th>
+          <th></th>
           <th>Aspek Penilaian 1</th>
           <th>Aspek Penilaian 2</th>
           <th>Aspek Penilaian 3</th>
@@ -17,13 +17,158 @@ function App() {
         <tbody>
           <tr class="Mahasiswa1">
             <td>Mahasiswa 1</td>
-            <td><input type="number" range="1" min="1" max="10"></td>
-            <td><input type="text"></td>
-            <td></td>
-            <td></td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 2</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 3</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 4</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 5</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 6</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 7</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 8</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 9</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+          </tr>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 10</td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
+            <td>
+              <input type="number" range="1" min="1" max="10" />
+            </td>
           </tr>
         </tbody>
       </table>
+
+      <button type="button">Simpan</button>
     </div>
   );
 }
