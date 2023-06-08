4de478839063daca5c345b008f134e64c8da8cfe
Zhephyr <ilhamsyawal87@gmail.com>
2023-06-08 19:37:13 +0700

    casdas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Aplikasi Penilaian Mahasiswa</h1>
+
+      <table>
+        <thead>
+          <th>Mahasiswa Ke-X</th>
+          <th>Aspek Penilaian 1</th>
+          <th>Aspek Penilaian 2</th>
+          <th>Aspek Penilaian 3</th>
+          <th>Aspek Penilaian 4</th>
+        </thead>
+
+        <tbody>
+          <tr class="Mahasiswa1">
+            <td>Mahasiswa 1</td>
+            <td><input type="number" range="1" min="1" max="10"></td>
+            <td><input type="text"></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
